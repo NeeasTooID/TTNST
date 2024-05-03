ece5952c5d7cb01822241af19f345c847f6d9da6
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-05-03 10:39:46 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { createGetInitialProps } from '@mantine/next';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { Analytics } from '@vercel/analytics/react';
 
 const getInitialProps = createGetInitialProps();
 
@@ -14,6 +15,7 @@ export default class _Document extends Document {
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     );

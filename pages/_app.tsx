fc21baca1cec2e9002261d97147b3a28dbfdd212
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-05-09 05:53:18 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Poppins } from "next/font/google";
 import Head from 'next/head';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   weight: ['500', '400', '900'],
@@ -14,7 +14,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>tikdown</title>
+        <title>Snaptik NST</title>
+        <link rel="icon" type="image/png" href="/icons/favicon.png" />
       </Head>
       <MantineProvider
         theme={{

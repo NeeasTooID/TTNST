7b927ef6013f711a70c00d96ae0d2f790e9cf141
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-05-09 05:58:44 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>tikdown</title>
         <link rel="icon" type="image/png" href="/icons/favicon.png" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5847575365620125" crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5847575365620125" crossOrigin="anonymous"></script>
       </Head>
       <MantineProvider
         theme={{
